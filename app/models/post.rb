f28ea496f26715f3940252714445f90608dcf926
Luis Pomare <luispomare92@gmail.com>
2023-06-07 11:33:47 -0500

    Fix linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,10 @@ class Post < ApplicationRecord
   has_many :coments
   has_many :likes
   after_save :increment_post_counter
-  
+
   def last_comments(reviewed_post = self)
     Comment.where(post: reviewed_post).order(created_at: :desc).limit(5)
   end
-
 
   def increment_post_counter
     author.increment!(:post_counter)
