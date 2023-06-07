0790b4ea8781a3becf9a2517db5ad948f3c4564f
Luis Pomare <luispomare92@gmail.com>
2023-06-07 11:32:32 -0500

    Create post model

---
+++ app/models/post.rb
@@ -0,0 +1,15 @@
+class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :coments
+  has_many :likes
+  after_save :increment_post_counter
+  
+  def last_comments(reviewed_post = self)
+    Comment.where(post: reviewed_post).order(created_at: :desc).limit(5)
+  end
+
+
+  def increment_post_counter
+    author.increment!(:post_counter)
+  end
+end
