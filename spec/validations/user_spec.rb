d1be6b2ea92c1ae7075e9265c654eb0bf3c1eed2
Luis Pomare <luispomare92@gmail.com>
2023-06-08 17:04:15 -0500

    Fix factory bot location

--- spec/validations/user_spec.rb
+++ spec/validations/user_spec.rb
@@ -10,4 +10,15 @@ describe User do
       expect(User.create(name: 'Ariel', photo: 'http/foto.com', bio: 'bio', post_counter: -3).valid?).to eq(false)
     end
   end
+
+  describe 'method:' do
+    it 'last_post' do
+      user = User.create(name: 'Ariel', photo: 'http/foto.com', bio: 'bio', post_counter: 3)
+      post1 = FactoryBot.create(:post, title: 'post_1', author: user, comments_counter: 0, likes_counter: 0)
+      post2 = FactoryBot.create(:post, title: 'post_2', author: user, comments_counter: 0, likes_counter: 0)
+      post3 = FactoryBot.create(:post, title: 'post_3', author: user, comments_counter: 0, likes_counter: 0)
+
+      expect(user.last_posts).to eq([post3, post2, post1])
+    end
+  end
 end
