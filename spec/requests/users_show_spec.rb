6e2d0ecf26d6094f111af3bf396a84d8804f2ba4
Luis Pomare <luispomare92@gmail.com>
2023-06-12 17:16:57 -0500

    Create user/show specs

---
+++ spec/requests/users_show_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Users/show', type: :request do
+  describe 'GET /users/:user_id' do
+    let!(:user) { User.create(name: 'Ariel', post_counter: 0) }
+
+    before :each do
+      get user_path(:user)
+    end
+
+    it 'should have a http status of 200(correct status)' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'should render users/show view' do
+      expect(response).to render_template('users/show')
+    end
+
+    it 'should include the placeholder text' do
+      expect(response.body).to include('Users show')
+    end
+  end
+end
