21ac3865a057027364927f687af9328ee444c55a
Luis Pomare <luispomare92@gmail.com>
2023-06-13 16:07:00 -0500

    Create basic user show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show ]
+  
   def index
     @users = User.all
   end
 
   def show; end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def user_params
+      params.require(:user).permit(:name, :photo, :string, :bio, :post_counter)
+    end
 end
