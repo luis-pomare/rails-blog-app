28cb2cf9776967f31707826646cf50e88ed27a47
Luis Pomare <luispomare92@gmail.com>
2023-06-14 12:50:21 -0500

    Recieve user params in post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
+  before_action :set_user, only: %i[ index ]
   def index; end
 
   def show; end
+
+  private
+  def set_user
+    @user = User.find(params[:user_id])
+  end
 end
