1eb0ac957fb0123327f7ab85ab76def900d69c7e
Luis Pomare <luispomare92@gmail.com>
2023-06-15 00:39:28 -0500

    Fix linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show ]
-  
+  before_action :set_user, only: %i[show]
+
   def index
     @users = User.all
   end
@@ -10,13 +10,14 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:name, :photo, :string, :bio, :post_counter)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:name, :photo, :string, :bio, :post_counter)
+  end
 end
