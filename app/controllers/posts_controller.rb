b00ffd133128015a7df6790ae68223b85f11433b
Luis Pomare <luispomare92@gmail.com>
2023-06-14 19:29:16 -0500

    Create set post private post_controller method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,17 @@
 class PostsController < ApplicationController
-  before_action :set_user, only: %i[ index ]
+  before_action :set_user, :set_post
   def index
-    @posts = Post.where(author: @user)
   end
 
-  def show; end
+  def show
+  end
 
   private
   def set_user
     @user = User.find(params[:user_id])
+  end
+  
+  def set_post
+    @posts = Post.where(author: @user)
   end
 end
