cdb0a79f21e7a268926375923f334c7b5e132682
Luis Pomare <luispomare92@gmail.com>
2023-06-16 14:40:13 -0500

    Add post_controller create method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,14 +15,10 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
 
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.save
+      redirect_to post_url(@post), notice: "Post was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
