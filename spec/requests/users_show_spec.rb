7c7548aeaa6039e70b19766b8776db77c3e4668e
Luis Pomare <luispomare92@gmail.com>
2023-06-18 08:52:52 -0500

    Fix users_show_spec

--- spec/requests/users_show_spec.rb
+++ spec/requests/users_show_spec.rb
@@ -2,19 +2,21 @@ require 'rails_helper'
 
 RSpec.describe 'Users/show', type: :request do
   describe 'GET /users/:user_id' do
-    let!(:user) { User.create!(name: 'Ariel', post_counter: 0) }
+    let!(:user) {User.create(name: 'Ariel', photo: 'http/foto.com', bio: 'bio', post_counter: 3)}
+    before :each do
+      get user_path(user)
+    end
 
     it 'should have a http status of 200(correct status)' do
-      get '/users/1'
       expect(response).to have_http_status(200)
     end
 
-    # it 'should render users/show view' do
-    #   expect(response).to render_template('users/show')
-    # end
+    it 'should render users/show view' do
+      expect(response).to render_template('users/show')
+    end
 
-    # it 'should include the placeholder text' do
-    #   expect(response.body).to include('Bio')
-    # end
+    it 'should include the placeholder text' do
+      expect(response.body).to include('Ariel')
+    end
   end
 end
