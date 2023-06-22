eaa23205620b75c84df0915bf2ca7e0e3669103d
David Espino García <david@MacBook-Air-de-David.local>
2023-06-22 14:35:28 -0600

    Add a test for add comment endpoint

--- spec/requests/api/api_spec.rb
+++ spec/requests/api/api_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'api/api', type: :request do
     let!(:user) { User.create(name: 'Ariel', photo: 'http/foto.com', bio: 'bio', post_counter: 3, email: 'mail@mail.com', password: 'password') }
     it 'should have a successful status' do 
       get "/api/users/#{user.id}/posts"
-      expect(response.status).to be(200)
+      expect(response).to have_http_status(200)
     end
   end
   describe '/api/posts/:post_id/comments' do 
@@ -17,4 +17,19 @@ RSpec.describe 'api/api', type: :request do
       expect(response.status).to be(200)
     end
   end
+
+  describe '/api/comment/new' do 
+    let!(:user) { User.create(name: 'Ariel', photo: 'http/foto.com', bio: 'bio', post_counter: 3, email: 'mail@mail.com', password: 'password') }
+    let!(:post_one) { Post.create(title: 'Ruby on rails', author: user, comments_counter: 0, likes_counter: 0) }
+    it 'should have a success http status' do 
+      post '/api/comment/new', params: {
+        post_id: post_one.id, 
+        text: 'test text',
+        current_user: user.id
+      }
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+
 end
