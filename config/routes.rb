6509c4ebd3cdd31ee56dbf50a64a5b95d5ffd1cc
David Espino García <david@MacBook-Air-de-David.local>
2023-06-22 08:58:24 -0600

    Add user posts endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { confirmations: 'users/confirmations' }
   root 'users#index'
+  get '/api/users/:user_id/posts', to: 'api#user_posts'
+  get '/api/posts/:post_id/comments', to: 'api#post_comments'
   resources :users do
     resources :posts do
       resources :comments, only: [:new, :create]
